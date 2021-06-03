4b7b9ae2566f6ec3a18d8ac35fe896afe63e4992
serhii beznosiuk <ostem971@gmail.com>
2021-06-04 01:04:25 +0300

    Updates

---
+++ static/js/NotFound.fde01799.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[6],{94:function(t,e,i){"use strict";i.r(e);i(0);var n=i(1);e.default=function(){return Object(n.jsx)("h2",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=NotFound.fde01799.chunk.js.map
